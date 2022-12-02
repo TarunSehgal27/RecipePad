c52a19abac806943a4edf7e81377ddeee9171a1a
Tarun Sehgal <tarunsehgal27@gmail.com>
2022-12-02 11:50:16 +0530

    third commit

--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -2,19 +2,19 @@
 import React from "react";
 import ShowRecipes from './ShowRecipes';
 
-export default function Recipe() {
+export default function Recipe({ id, title, image }) {
     return (
         <div className="recipe-box">
             <div className="recipe-img">
-                <img src="./assets/recipe.jpg" height="100%" width="100%" alt="image of recipe"/>
+                <img src={image} height="100%" width="100%" alt="image of recipe"/>
             </div>
             <div className="recipe-description">
                 <div className="heading">
-                    <h6 className='id'>Unique ID : 12</h6>
-                    <h3 className="title">Scramble Eggs in the Microwave</h3>
-                    <p className="source">incredibleegg.org</p>
+                    
+                    <h3 className="title">{title}</h3>
+                    
                 </div>
-                <p className="numberOfIngredients">You have all 8 ingredients.</p>
+                
             </div>
         </div>
     );
--- src/Components/ShowRecipes.js
+++ src/Components/ShowRecipes.js
@@ -5,16 +5,22 @@ import Recipe from './Recipe';
 import axios from 'axios'
 
 export default function ShowRecipes() {
-    const [apiData,setapiData] = useState([])
+    const [apiData,setApiData] = useState([])
     useEffect(() =>{
       async function callApi(){
         const apiResult = await axios.get('http://127.0.0.1:9000/recipesWithSearch')
         
-        setapiData(apiResult.data.recipes)
+        setApiData(apiResult.data.recipes)
+        console.log(apiResult)
     }
     callApi()
     console.log(apiData)
       },[])
+
+      const recipeInformation = async (id) => {
+        const res = await axios.get(`http://localhost:9000/recipeInformation/${id}`)
+        console.log(res,id)
+      }
     return (
         <div>
             <h1>RecipePad</h1>
@@ -22,8 +28,7 @@ export default function ShowRecipes() {
             <div className="searchbox-recipes">
                 <input type='search' placeholder='Search'/>
             </div>
-            {apiData.map(recipe=> console.log(recipe))
-            }
+            {apiData && apiData.map(recipe=> <div onClick={() => recipeInformation(recipe.id)}><Recipe title={recipe.title} image={recipe.image} /></div>)}
         </div>
     );
 }
